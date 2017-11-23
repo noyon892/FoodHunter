7664f5b4a4a9282d530d9e238c361d85cca0bec8
Hasanuzzaman Noyon <noyon892@gmail.com>
2017-11-24 01:48:34 +0600

    News Partially and Restaurant_Admin Layout complete

--- FoodHunter.FoodHunterWeb/FoodHunter.FoodHunterWeb.AppLayer/ViewModels/Create/NewsCreateViewModel.cs
+++ FoodHunter.FoodHunterWeb/FoodHunter.FoodHunterWeb.AppLayer/ViewModels/Create/NewsCreateViewModel.cs
@@ -1,8 +1,19 @@
-﻿using FoodHunter.FoodHunterWeb.AppLayer.ViewModels.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using FoodHunter.FoodHunterWeb.AppLayer.ViewModels.Base;
+using FoodHunter.Web.DataLayer;
+using System.Collections.Generic;
 
 namespace FoodHunter.Web.AppLayer.ViewModels.Create
 {
     public class NewsCreateViewModel : NewsBaseViewModel
     {
+        public List<Restaurant> Restaurants { get; set; }
+        public int RestaurantId { get; set; }
+
+        public NewsCreateViewModel() : base()
+        {
+            
+        }
     }
 }
